621662e62ce9f1f76ea61ce82e465773b10d8f2e
checks01 <isc.sva@gmail.com>
2020-01-16 16:12:45 -0600

    cors

--- src/main/java/mx/com/lobos/msc/bincoServices/funciones/UsuariosDao.java
+++ src/main/java/mx/com/lobos/msc/bincoServices/funciones/UsuariosDao.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 import mx.com.lobos.msc.bincoServices.entidades.Usuarios;
 
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RepositoryRestResource(path = "usuarios")
 public interface UsuariosDao extends PagingAndSortingRepository<Usuarios, BigDecimal> {
 
--- src/main/java/mx/com/lobos/msc/bincoServices/usuarios/FuncionesDao.java
+++ src/main/java/mx/com/lobos/msc/bincoServices/usuarios/FuncionesDao.java
@@ -4,9 +4,12 @@ import java.math.BigDecimal;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import mx.com.lobos.msc.bincoServices.entidades.Funciones;
 
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RepositoryRestResource(path = "funciones")
 public interface FuncionesDao extends PagingAndSortingRepository<Funciones, BigDecimal> {
 
